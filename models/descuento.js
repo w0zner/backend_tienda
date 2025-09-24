b15a83a46bd6252fa771b2fc8b92b592f4f6d4e8
cesar_ramirez <cesar_ramirez@hacienda.gov.py>
2025-09-24 16:54:00 -0300

    fix campo descuento

--- models/descuento.js
+++ models/descuento.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const DescuentoSchema = new mongoose.Schema({
     titulo: {type: String, required: true},
     banner: {type: String, required: true},
-    descuento: {type: String, required: true},
+    descuento: {type: Number, required: true},
     fecha_inicio: {type: String, required: true},
     fecha_fin: {type: String, required: true},
     createdAt: {type: Date, default: Date.now}
