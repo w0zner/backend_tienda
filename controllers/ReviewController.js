be04fd778406ace6df75706495f0cf5c3cda3837
Rodrigo Ramirez <cesarrodrigoramirez@gmail.com>
2025-11-10 23:30:47 -0300

    avance en resenha

--- controllers/ReviewController.js
+++ controllers/ReviewController.js
@@ -1,30 +1,62 @@
-const Review = require('../models/review');
+const Review = require("../models/review");
 
 const obtenerPorId = async (req, res) => {
-    try {
-        const id = req.params.id
-        const review = await Review.findById(id)
+  try {
+    const id = req.params.id;
+    const review = await Review.findById(id);
 
-        res.json({data: review})
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({message: 'Error '+error.message})
-    }
-}
+    res.json({ data: review });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error " + error.message });
+  }
+};
+
+const obtenerPorVentaProducto = async (req, res) => {
+  try {
+    const venta = req.params.venta;
+    const producto = req.params.producto;
+
+    const review = await Review.find({ venta: venta, producto: producto }).sort(
+      { createdAt: -1 },
+    );
+
+    res.json({ data: review });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error " + error.message });
+  }
+};
 
 const guardar = async (req, res) => {
-    try {
-        const body = req.body
+  try {
+    const body = req.body;
 
-        const review = await Review.create(body)
+    const review = await Review.create(body);
 
-        res.status(200).json({ data: review})
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({
-            message: 'Error ' + error.message
-        })
-    }
-}
+    res.status(200).json({ data: review });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Error " + error.message,
+    });
+  }
+};
 
-module.exports = { obtenerPorId, guardar }
+const update = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const body = req.body;
+
+    const review = await Review.findByIdAndUpdate(id, body, { new: true });
+
+    res.status(200).json({ data: review });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Error " + error.message,
+    });
+  }
+};
+
+module.exports = { obtenerPorId, guardar, obtenerPorVentaProducto, update };
--- routes/review.js
+++ routes/review.js
@@ -1,13 +1,17 @@
-const express = require('express')
-const reviewController = require('../controllers/ReviewController')
-const authenticate = require('../middlewares/authenticate')
+const express = require("express");
+const reviewController = require("../controllers/ReviewController");
+const authenticate = require("../middlewares/authenticate");
 
-const api = express.Router()
+const api = express.Router();
 
-//api.get('/', authenticate.verifyAuthAdmin, cuponController.listar)
-api.get('/:id', authenticate.verifyAuthAdmin, reviewController.obtenerPorId)
-api.post('/', authenticate.verifyAuthAdmin, reviewController.guardar)
-//api.put('/:id', authenticate.verifyAuthAdmin, cuponController.actualizar)
+api.get(
+  "/:venta/:producto",
+  authenticate.verifyAuth,
+  reviewController.obtenerPorVentaProducto,
+);
+api.get("/:id", authenticate.verifyAuth, reviewController.obtenerPorId);
+api.post("/", authenticate.verifyAuth, reviewController.guardar);
+api.put("/:id", authenticate.verifyAuth, reviewController.update);
 //api.delete('/:id', authenticate.verifyAuthAdmin, cuponController.eliminar)
 
-module.exports = api
+module.exports = api;
